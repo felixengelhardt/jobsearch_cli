8861e74b7d65c72a6f0f4420deca5d7a09a03a57
felixengelhardt <fengelh@gwdg.de>
2018-03-04 13:21:37 +0100

    Working Version. URLS still need to be parsed.

---
+++ jobsearch_cli.py
@@ -0,0 +1,115 @@
+from urllib import request
+from bs4 import BeautifulSoup
+import numpy as np
+import re
+import os
+import sys
+import logging
+import argparse
+
+engineUrls = {
+    'monster': 'https://www.monster.de/jobs/suche/?q={}&where={}&cy=de&rad={}&sort=dt.rv.di&page=1',
+    'indeed': 'https://de.indeed.com/Jobs?as_and={}&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&st=&l={}&radius={}&fromage=&limit=&sort=&psf=advsrch',
+    'stepstone': 'https://www.stepstone.de/5/ergebnisliste.html?stf=freeText&ns=1&qs=%5B%5D&companyID=0&cityID=0&sourceOfTheSearchField=homepagemex%3Ageneral&searchOrigin=Homepage_top-search&ke={}&ws={}&ra={}'}
+
+getTags = {
+    'monster': [('div', 'jobTitle'), ('div', 'company'), ('div', 'job-specs job-specs-location')],
+    'stepstone': [('h2', 'job-element__body__title'),
+        ('div', 'job-element__body__company'),
+        ('li', 'job-element__body__location')]} # Stepstone URL ', ('a', 'job-element__url')'
+
+logging.basicConfig(filename='jobsearch.log', level=logging.DEBUG)
+
+def getJobs(engine, jobname, location, radius):
+    print('Checking for jobs on {}:\n'.format(engine))
+    logging.warning('Input für Funktion getJobs {} {} {} {}'.format(engine, jobname, location, radius))            
+    logging.debug(engineUrls[engine].format(jobname, location, radius))
+    page = request.urlopen(engineUrls[engine].format(jobname, location, radius))
+    bs4Page = BeautifulSoup(page, 'lxml')
+    jobs = []
+    if engine == 'stepstone':
+        test = bs4Page.find_all('a', class_='job-element__url', href=True)
+        logging.warning([x.get('href') for x in test])
+    #
+    # bs4 gets indeed non-premium job ads as javascript snippet 
+    # therefore special treatment is necessary
+    #
+    if engine == 'indeed':
+        title = []
+        company = []
+        city = []
+        url = []
+        try:
+            indeed = bs4Page.find('script', text=re.compile('jobmap')).text
+            indeedJobs = re.findall('jobmap\[.*\].*=.*\{.*\}', indeed)
+            indeedJobs = [ re.sub('jobmap\[.*\].*=.*\{','',job) for job in indeedJobs ]
+            indeedJobs = [ re.sub('\}','',job) for job in indeedJobs ]
+            indeedJobs = [ job.split(',') for job in indeedJobs ]
+            for job in indeedJobs:
+                for entry in job:
+                    if entry.startswith('title:'):
+                        title.append(entry.split(':')[1].strip("'"))
+                    elif entry.startswith('cmp:'):
+                        company.append(entry.split(':')[1].strip("'"))
+                    elif entry.startswith('city:'):
+                        city.append(entry.split(':')[1].strip("'"))
+                    elif entry.startswith('jk:'):
+                        url.append('https://de.indeed.com/rc/clk?jk={}'.format(entry.split(':')[1].strip("'")))
+            jobs.append(title)
+            jobs.append(company)
+            jobs.append(city)        
+        except AttributeError:
+            print('Something went wrong. There are no jobs here.\n')
+            pass       
+    else:
+        for entry in getTags[engine]:
+            jobs.append([x.get_text().strip() for x in bs4Page.find_all(entry[0],
+                class_=entry[1])])
+    jobs = np.array(jobs).T.tolist()
+    return jobs
+
+
+def main(engines, jobname, location, radius):
+    engines = engines.strip().split(',')
+    if 'all' in engines:
+        engines = ['monster', 'indeed', 'stepstone']
+    jobs = {}
+    for engine in engines:
+        jobs[engine] = getJobs(engine, jobname, location, radius)
+        for job in jobs[engine][0:9]:
+            print(u'Job Title: {}\nCompany: {}\nLocation: {}\n'.format(job[0], job[1], job[2]))
+
+
+if __name__ == '__main__':
+    if os.path.isfile('/home/felix/.jobsearch_cli'):
+        f = open('/home/felix/.jobsearch_cli','r')
+        jobTitle = f.readline().split('=')[1].strip()
+        location = f.readline().split('=')[1].strip()
+        radius = f.readline().split('=')[1].strip()
+        logging.debug('{} {} {}'.format(jobTitle, location, radius))
+        f.close()        
+    else:
+        print('No config with defaults found. Please enter your default search queries')
+        title = input('Please enter job title: ')
+        location = input('Please enter location: ')
+        radius = input('Please enter radius around location which is acceptable: ')
+        f = open('/home/felix/.jobsearch_cli','w')
+        f.write('title={}\nlocation={}\nradius={}'.format(title, location, radius))
+        f.close()
+        sys.exit()
+    parser = argparse.ArgumentParser(description='Jobsearch on the Commandline\
+        with Python.')
+    parser.add_argument(
+        '-j', '--jobTitle', help='Title of the job you are looking for', default='{}'.format(jobTitle))
+    parser.add_argument(
+        '-l', '--location', help='Location you want to your job to be', default='{}'.format(location))
+    parser.add_argument(
+        '-r', '--radius', help='Search radius around entered location', default='{}'.format(radius))
+    parser.add_argument(
+        '-e', '--engine', help='Set the job search engine. Default value is \
+        all, valid values monster, indeed, stepstone, all. Use a comma to \
+        seperate engines',
+        default='all')
+    args = parser.parse_args()
+    logging.debug('Suchkriterien: {} {} {} {}'.format(args.engine, args.jobTitle, args.location, args.radius))
+    main(args.engine, args.jobTitle, args.location, args.radius)
